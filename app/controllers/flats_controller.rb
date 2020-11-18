8891a72aae002490be851fd08d1fab4ab57d374f
flaviomahoney <flavio@mahoneyt.com>
2020-11-18 17:20:02 -0300

    Did some refactoring

--- app/controllers/flats_controller.rb
+++ app/controllers/flats_controller.rb
@@ -16,38 +16,28 @@ class FlatsController < ApplicationController
     end
   end
 
-  # GET /flats/1
-  # GET /flats/1.json
   def show
   end
 
-  # GET /flats/new
   def new
     @flat = Flat.new
   end
 
-  # GET /flats/1/edit
   def edit
   end
 
-  # POST /flats
-  # POST /flats.json
   def create
     @flat = Flat.new(flat_params)
 
     respond_to do |format|
       if @flat.save
         format.html { redirect_to @flat, notice: 'Flat was successfully created.' }
-        format.json { render :show, status: :created, location: @flat }
       else
         format.html { render :new }
-        format.json { render json: @flat.errors, status: :unprocessable_entity }
       end
     end
   end
 
-  # PATCH/PUT /flats/1
-  # PATCH/PUT /flats/1.json
   def update
     respond_to do |format|
       if @flat.update(flat_params)
@@ -60,8 +50,6 @@ class FlatsController < ApplicationController
     end
   end
 
-  # DELETE /flats/1
-  # DELETE /flats/1.json
   def destroy
     @flat.destroy
     respond_to do |format|
@@ -71,12 +59,10 @@ class FlatsController < ApplicationController
   end
 
   private
-    # Use callbacks to share common setup or constraints between actions.
     def set_flat
       @flat = Flat.find(params[:id])
     end
-
-    # Only allow a list of trusted parameters through.
+    
     def flat_params
       params.require(:flat).permit(:name, :address)
     end
